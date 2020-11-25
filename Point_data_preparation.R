70289a0e82b04454a7c95500b87a67a94ae08660
Eduardo Arlé <eduardo.arle@idiv.de>
2020-11-25 11:53:59 +0100

    Point simulation

--- Point_data_preparation.R
+++ Point_data_preparation.R
@@ -1,10 +1,14 @@
 #load necessary packages
 
-library(raster);library(rgdal);library(rgeos)
+library(raster);library(rgdal);library(rgeos);library(rworldmap)
 
 #set paths to files that will be used (species list available in Supplementary Information)
 
 wd_points <- "C:/Users/ca13kute/Documents/bRacatus/bRacatus_data/GBIF_occurrence_not_process"
+wd_suit_layers <- "C:/Users/ca13kute/Documents/bRacatus/bRacatus_data/Suitability_layers"
+wd_ranges <- "C:/Users/ca13kute/Documents/bRacatus/bRacatus_data/Range_maps"
+wd_ecorregions <- "C:/Users/ca13kute/Documents/bRacatus/bRacatus_data/Ecoregions"
+wd_sim_pts <- "C:/Users/ca13kute/Documents/bRacatus/bRacatus_data/GBIF_occurrence"
 
 #import GBIF point records
 
@@ -59,243 +63,123 @@ for(i in 1:length(gbif6))
   saveRDS(gbif6[[i]],sps_gbif[i])
 }
 
-#load suitability maps for each species in its native range
+eco_reg <- readOGR(dsn=wd_ecorregions,layer="wwf_terr_ecos") #load terrestrial ecoregions shp
 
-setwd("/gpfs1/data/idiv_meyer/suitable_habitat/Esa_10km_NoElev_suit_unWeight/mamm/2015")
-
-suit_lst <- list.files()
-
-# load maps that are available for the selected species
-
-suit_maps <- list()
 for(i in 1:length(gbif6))
 {
-  a <- grep(names(gbif6)[i],suit_lst)
-  if(length(a)!=0){
-    suit_maps[[length(suit_maps)+1]] <- raster(suit_lst[a])
-    names(suit_maps)[length(suit_maps)] <- names(gbif6)[i]
-  }
-  print(i)
-}
+  pts <- gbif6[[i]]
 
-suit_hab2 <- suit_maps
+  setwd(wd_suit_layers) #load sps suitability map
+  suit <- raster(paste0(sps_gbif[i],".tif"))
 
-i=1
-for(i in 1:length(suit_hab2))   #transform 0 values in NA
-{
-  suit_hab2[[i]][] <- ifelse(suit_hab2[[i]][]==0,NA,suit_hab2[[i]][])
-  suit_hab2[[i]] <- aggregate(suit_hab2[[i]],fact=6)
-  print(i)
-}
+  range <- readOGR(sps_names[i],dsn=wd_ranges) #load range map for the species
 
-### save simplified mammal suitability maps
+  #select ecoregions out of ranges
+  eco_reg_abs <- eco_reg[which(eco_reg$OBJECTID %ni% eco_reg[subset(range),]$OBJECTID),]
+  #seed half the number of presence gbif points out of the ecoregions
+  random_pts <- spsample(eco_reg_abs,n=ceiling(nrow(pts_pr)/2),type="random")
+  #prepare and add the random points to gbif data
+  coords_rpts <- as.data.table(coordinates(random_pts))
+  names(coords_rpts) <- c("decimalLongitude","decimalLatitude")
+  random_pts_ID <- extract(ID_raster,random_pts) #extract cell ID from each gbif record
+  final_rpts <- cbind(species=gsub("_"," ",sps_names[i]),coords_rpts,occurrenceStatus="simulated",
+                      type="Random_false",cell_ID=random_pts_ID)
+  pts2 <- rbind(pts[,-1],final_rpts)
 
-setwd("/gpfs1/data/idiv_meyer/01_projects/eduardo/Suitability_selected_mammals_half_degree")
+  #crop the suitability maps by the range plus one degree buffer
 
-mapply(function(x,y) writeRaster(x,filename=y,datatype="img",overwrite=F),
-       suit_hab2,names(suit_hab2))
+  range_buffer <- gBuffer(range,width=1) #create a buffer of 1 degree on the range map
 
-# suit_hab3 <- lapply(suit_hab2,function(x){aggregate(x,fact=6)})   #aggregate cells to 0.5 resolution
+  a <- extend(suit2,extent(range_buffer),snap="out")
+  b <- rasterize(range_buffer,a) #create a mask to crop the raster
+  suit3 <- mask(a,mask=b)
 
+  #get coordinates and values of cells with suitability for the species
 
-#load, simplify and save range maps per group (in parallel sessions)
+  suit4 <- as.data.table(rasterToPoints(suit3))
 
-wd_anura <- "/gpfs1/data/idiv_meyer/01_projects/eduardo/Range_maps/AMPHIBIANS/ANURA/Anura_ranges_sps"
-wd_anura2 <- "/gpfs1/data/idiv_meyer/01_projects/eduardo/Range_maps/AMPHIBIANS/ANURA/Anura_ranges_sps_simp"
-
-range_anura <- list()
-for(i in 1:length(gbif6))
-{
-  a <- try(readOGR(dsn=wd_anura,layer=gsub("_"," ",names(gbif6)[i])),silent=T)
-  if(class(a)!="try-error"){
-    b <- gSimplify(a,.1,topologyPreserve=T)
-    c <- SpatialPolygonsDataFrame(b, data=a@data)
-    range_anura[[length(range_anura)+1]] <- c
-    names(range_anura)[length(range_anura)] <- names(gbif6)[i]
-    writeOGR(c,dsn=wd_anura2,layer=names(gbif6)[i],driver="ESRI Shapefile")
-  }
-  print(i)
-}
-
-wd_mammal <- "/gpfs1/data/idiv_meyer/01_projects/eduardo/Range_maps/TERRESTRIAL_MAMMALS/Terrestrial_mammals_ranges_sps"
-wd_mammal2 <- "/gpfs1/data/idiv_meyer/01_projects/eduardo/Range_maps/TERRESTRIAL_MAMMALS/Terrestrial_mammals_ranges_sps_simp"
-
-range_mammal <- list()
-for(i in 1:length(gbif6))
-{
-  a <- try(readOGR(dsn=wd_mammal,layer=gsub("_"," ",names(gbif6)[i])),silent=T)
-  if(class(a)!="try-error"){
-    b <- gSimplify(a,.1,topologyPreserve=T)
-    c <- SpatialPolygonsDataFrame(b, data=a@data)
-    range_mammal[[length(range_mammal)+1]] <- c
-    names(range_mammal)[length(range_mammal)] <- names(gbif6)[i]
-    writeOGR(c,dsn=wd_mammal2,layer=names(gbif6)[i],driver="ESRI Shapefile")
-  }
-  print(i)
-}
-
-wd_aves <- "/gpfs1/data/idiv_meyer/01_projects/eduardo/Range_maps/AVES/Aves_ranges_sps"
-wd_aves2 <- "/gpfs1/data/idiv_meyer/01_projects/eduardo/Range_maps/AVES/Aves_ranges_sps_simp"
-
-range_aves <- list()
-for(i in 1:length(gbif6))
-{
-  a <- try(readOGR(dsn=wd_aves,layer=gsub("_"," ",names(gbif6)[i])),silent=T)
-  if(class(a)!="try-error"){
-    b <- gSimplify(a,.1,topologyPreserve=T)
-    c <- SpatialPolygonsDataFrame(b, data=a@data)
-    range_aves[[length(range_aves)+1]] <- c
-    names(range_aves)[length(range_aves)] <- names(gbif6)[i]
-    writeOGR(c,dsn=wd_aves2,layer=names(gbif6)[i],driver="ESRI Shapefile")
-  }
-  print(i)
-}
-
-
-
-range_mam <- "C:\\Users\\ca13kute\\Documents\\GIT_projects\\Range_maps\\Mammals_corrected"
-
-setwd(range_mam)
-range_lst <- gsub(".shp","",list.files(pattern="shp$"))
-
-ranges <- lapply(range_lst,FUN=function(x){
-  readOGR(dsn=range_mam,layer=x)})
-
-sps <- unique(gsub("(^.*)_.*$","\\1",range_lst)) #list unique sps names
-
-
-#create one map per sps with only native range
-
-ranges_sps_native <- lapply(range_lst[grep(pattern="_native$",range_lst)],function(x){
-  readOGR(dsn=range_mam,layer=x)})
-
-ranges_sps_native2 <- lapply(ranges_sps_native,function(x){
-  gSimplify(x,.1)})    #simplify the polygon lines
-
-#create one map per sps (native,alien and unknown)
-
-ranges_sps <- list()
-i=1
-for(i in 1:length(sps))
-{
-  a <- ranges[grep(sps[i],range_lst)]
-  if(length(a)>1){
-    ranges_sps[[i]] <- a[[1]]
-    for(j in 2:length(a))
-    {
-      ranges_sps[[i]] <- gUnion(ranges_sps[[i]],a[[j]])
-      ranges_sps[[i]]$data <- 0
-    }
+  #create max of 3/12 GBIF number as pseudo false records within cells with 0% suitability
+  if(length(which(suit4[,3]==0))==0){
+    rows <- 0
   }else{
-    ranges_sps[[i]] <- a[[1]]
+    rows <-as.numeric(unique(sample(as.character(which(suit4[,3]==0)),
+                                    ceiling(nrow(pts_pr)*3/12),replace=T)))
   }
+  pseudo_false1 <- suit4[rows,]
+  names(pseudo_false1) <- c("decimalLongitude","decimalLatitude","suitability")
+
+  #create max of 2/12 GBIF number as pseudo false records within cells with less than 25% suitability
+  if(length(which(suit4[,3]<=25))==0){
+    rows <- 0
+  }else{
+    rows <-as.numeric(unique(sample(as.character(which(suit4[,3]<=25)),
+                                    ceiling(nrow(pts_pr)*2/12),replace=T)))
+  }
+  pseudo_false2 <- suit4[rows,]
+  names(pseudo_false2) <- c("decimalLongitude","decimalLatitude","suitability")
+
+  #create max of 1/12 GBIF number as pseudo false records within cells with between 25 and 50% suitability
+  #create max of 1/12 GBIF number as pseudo true records within cells with between 25% and 50% suitability
+  if(length(which(suit4[,3]>25 & suit4[,3]<=50))==0){
+    rows <- 0
+  }else{
+    rows <- as.numeric(unique(sample(as.character(which(suit4[,3]>25 & suit4[,3]<=50)),
+                                     ceiling(nrow(pts_pr)*2/12),replace=T)))
+  }
+  a <- sample(seq_len(length(rows)),size=ceiling(1/2*length(rows)))
+  pseudo_false3 <- suit4[as.numeric(rows[-a]),]
+  names(pseudo_false3) <- c("decimalLongitude","decimalLatitude","suitability")
+  pseudo_true1 <- suit4[as.numeric(rows[a]),]
+  names(pseudo_true1) <- c("decimalLongitude","decimalLatitude","suitability")
+
+  #create max of 2/12 GBIF number as pseudo true records within cells with between 50% and 75% suitability
+  if(length(which(suit4[,3]>50 & suit4[,3]<=75))==0){
+    rows <- 0
+  }else{
+    rows <- as.numeric(unique(sample(as.character(which(suit4[,3]>50 & suit4[,3]<=75)),
+                                     ceiling(nrow(pts_pr)*2/12),replace=T)))
+  }
+  pseudo_true2 <- suit4[rows,]
+  names(pseudo_true2) <- c("decimalLongitude","decimalLatitude","suitability")
+
+  #create max of 3/12 GBIF number as pseudo true records within cells with more than 75% suitability
+  if(length(which(suit4[,3]>75))==0){
+    rows <- 0
+  }else{
+    rows <-as.numeric(unique(sample(as.character(which(suit4[,3]>75)),
+                                    ceiling(nrow(pts_pr)*3/12),replace=T)))
+  }
+  pseudo_true3 <- suit4[rows,]
+  names(pseudo_true3) <- c("decimalLongitude","decimalLatitude","suitability")
+
+  #rbind all pseudo_true and all pseudo_false points per species
+  pseudo_true <- rbind(pseudo_true1,pseudo_true2,pseudo_true3)
+  pseudo_false <- rbind(pseudo_false1,pseudo_false2,pseudo_false3)
+  #create spatial points from pseudo_true and pseudo_false
+  pseudo_true_sp <- as.data.frame(pseudo_true)
+  if(nrow(pseudo_true_sp)>0){
+    coordinates(pseudo_true_sp) <- ~decimalLongitude+decimalLatitude
+    proj4string(pseudo_true_sp) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+    #get cell_ID of pseudo_true points
+    pseudo_true_ID <- extract(ID_raster,pseudo_true_sp)
+    #standardise pseudo_true points to gbif
+    final_pseudo_true <- cbind(species=sps_names[i],pseudo_true[,c(1,2),],occurrenceStatus="simulated",type="Pseudo_true",cell_ID=pseudo_true_ID)
+    #join gbif and pseudo_true points
+    pts2 <- rbind(pts2,final_pseudo_true)
+  }
+  pseudo_false_sp <- as.data.frame(pseudo_false)
+  if(nrow(pseudo_false_sp)>0){
+    coordinates(pseudo_false_sp) <- ~decimalLongitude+decimalLatitude
+    proj4string(pseudo_false_sp) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+    #get cell_ID of pseudo_false points
+    pseudo_false_ID <- extract(ID_raster,pseudo_false_sp)
+    #standardise pseudo_false points to gbif
+    final_pseudo_false <- cbind(species=sps_names[i],pseudo_false[,c(1,2)],occurrenceStatus="simulated",type="Pseudo_false",cell_ID=pseudo_false_ID)
+    #join gbif and pseudo_false points
+    pts2 <- rbind(pts2,final_pseudo_false)
+  }
+
+  setwd(wd_sim_pts)
+  saveRDS(pts2,sps_names[i])
   print(i)
 }
-
-#fix bug due to holes in shp
-
-ranges_sps[[1]] <- gBuffer(ranges_sps[[1]],width=.1)
-ranges_sps[[1]]$data <- 0
-
-ranges_sps[[10]] <- gBuffer(ranges_sps[[10]],width=.1)
-ranges_sps[[10]]$data <- 0
-
-#############################################
-
-#####suit hab comparison###
-
-suit_hab2
-
-setwd(paste0(wd_local,"\\01_projects\\eduardo\\Suit_hab_2"))
-
-a <- list.files()
-
-suit_hab_ruben <- lapply(a,raster)
-
-par(mfrow=c(1,2))
-
-plot(suit_hab2[[19]])
-plot(ranges_sps[[19]],add=T)
-
-plot(suit_hab_ruben[[5]])
-plot(ranges_sps[[19]],add=T)
-
-
-##############################
-
-wd_suit <- paste0(wd,"/03_tests/ESHM_edu/habitat_maps_area")
-
-setwd(wd_suit)
-
-files <- list.files()
-
-
-maps <- list()
-for(i in 1:length(files))
-{
-  maps[[i]] <- raster(files[i])
-  print(i)
-}
-
-range <- list()
-for(i in 1:length(maps))
-{
-  range[[i]] <- range(maps[[i]][],na.rm=T)
-  print(i)
-}
-
-range[[100]]
-range(maps2[[10]][],na.rm=T)
-range(maps[[1]][])
-
-which(!is.na(maps2[[10]][]))
-
-a <- as.numeric(which(sapply(range,class)=="integer"))
-maps2 <- maps[a]
-
-plot(maps2[[8]])
-
-
-b <- c(1,4,5)
-class(a)
-
-plot(maps[[1]])
-
-class(test3)
-class(test)
-
-plot(test)
-plot(test2)
-plot(test3)
-
-big <- character()
-for(i in 1:ncell(test3))
-{
-  if(!is.na(test3[i])){
-    big[length(big)+1] <- test3[1]}
-  print(i)
-}
-
-
-#extract suitability index for GBIF points
-
-gbif_sp2 <- lapply(gbif6,as.data.frame)  #create spatial points from gbif records
-
-i=1
-for(i in 1:length(gbif_sp2))
-{
-  coordinates(gbif_sp2[[i]]) <- ~decimalLongitude+decimalLatitude
-  proj4string(gbif_sp2[[i]]) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-}
-
-gbif_suit <- mapply(function(x,y) extract(x,y), suit_hab3, gbif_sp2)
-
-gbif7 <- list()
-i=1
-for(i in 1:length(gbif6))
-{
-  gbif7[[i]] <- cbind(gbif6[[i]],gbif_suit[[i]])
-  names(gbif7[[i]])[6] <- "suitability"
-}
-
-
